ebf32429e713c146330f8fc93cbbb4e81c0e2a78
ZeinabEltaib534 <zenabeltaib534@gmail.com>
2025-10-05 22:08:45 +0300

    Update app.py
    Update app.py to work on Replit

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    print("✅ Loaded pickled weather model, starting Flask...")
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
+
